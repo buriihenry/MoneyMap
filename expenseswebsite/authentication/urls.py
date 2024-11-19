7a56f88b0173245d4a187339bc6d893bff25d23c
buriihenry <buriihenry@gmail.com>
2024-11-19 09:25:37 +0300

    Added #code

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,8 +1,12 @@
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValiadtionView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 
 
 urlpatterns = [
-    path('register', RegistrationView.as_view(), name="register")
+    path('register', RegistrationView.as_view(), name="register"),
+    path('validate-username', csrf_exempt(UsernameValiadtionView.as_view()),
+         name= "validate-username"),
+
 ]
