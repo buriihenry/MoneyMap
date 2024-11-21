eade53b24cec5f7b09cc7b1f6bd659fcae024d2f
buriihenry <buriihenry@gmail.com>
2024-11-21 23:12:00 +0300

    Just needed to add this code

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,13 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from .models import Category, Expense
+from django.contrib import messages
 
 @never_cache  # This will prevent caching of this view
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories = Category.objects.all()
     return render(request, 'expenses/index.html')
 
 @never_cache  # Prevent caching for this view as well
 @login_required(login_url='/authentication/login')
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date,
+                               category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
+
